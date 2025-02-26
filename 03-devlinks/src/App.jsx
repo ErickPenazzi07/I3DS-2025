777d57c600f5dec36f0701d5fa579ce4d18f7a44
DevErick <djerickdev@gmail.com>
2025-02-26 15:12:48 -0300

    FinalizaçãoDaAtividadeReact

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,31 +1,43 @@
 import "./App.css";
+import foto from "./img/download.jpg";
 
 import Perfil from "./components/perfil/perfil";
 import Switch from "./components/switch/Switch";
 import Links from "./components/links/Links";
 import SocialLinks from "./components/socialLinks/SocialLinks";
 import Rodape from "./components/rodape/Rodape";
+import fotoLight from "./img/download.jpg";
+import fotoDark from "./img/perfillight.jpg";
+import { useState } from "react";
+
 
 const App = () => {
+  const[isLight, setIsLight] = useState(true);
+  
+  const troca = () => {
+    setIsLight(!isLight);
+  }
+
+
   return (
-    <div id="App" className="">
-      <Perfil />
-      <Switch />
-
+    <div id="App" className={isLight && "light"}>
+      <Perfil fotoPerfil={isLight ? fotoLight : fotoDark}>@DevErick</Perfil>
+      <Switch troca={troca} islight={isLight}/>
+<div id="container">
       <ul>
-        <Links />
-        <Links />
-        <Links />
-        <Links />
+        <Links link={"https://www.google.com/"}>Google</Links>
+        <Links link={"https://www.instagram.com/"}>Instagram</Links>
+        <Links link={"https://www.github.com/"}>Portifólio</Links>
+        <Links link={"https://br.linkedin.com/"}>Projetos</Links>
       </ul>
-
-      <div id="socialLinks">
-        <SocialLinks />
-        <SocialLinks />
-        <SocialLinks />
-        <SocialLinks />
       </div>
-      <Rodape />
+      <div id="socialLinks">
+        <SocialLinks link={"https://www.google.com/"} icon={"logo-github"} />
+        <SocialLinks link={"https://www.instagram.com/"} icon={"logo-instagram"} />
+        <SocialLinks link={"https://www.youtube.com/"} icon={"logo-youtube"} />
+        <SocialLinks link={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
+      </div>
+      <Rodape>DevErick</Rodape>
     </div>
   );
 };
--- 03-devlinks/src/components/switch/Switch.jsx
+++ 03-devlinks/src/components/switch/Switch.jsx
@@ -1,10 +1,12 @@
 import styles from "./Switch.module.css";
 
-const Switch = () => {
+const Switch = ({troca, islight}) => {
   return (
-    <div id={styles.switch}>
+    <div className={islight ? styles.light : undefined}>
+    <div onClick={troca} id={styles.switch}>
       <button></button>
       <span></span>
+    </div>
     </div>
   );
 };
